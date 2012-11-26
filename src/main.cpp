8fcae93b82c816f909500250fe8e474c4438d6d6
Yamakaky <yamakaky@gmail.com>
2012-11-26 11:13:43 +0100

    Ajout de la traduction des boutons Qt

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,20 @@
 #include <QtGui/QApplication>
 #include "fenetreprincipale.h"
 #include <QTranslator>
+#include <QLibraryInfo>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-
     QString locale = QLocale::system().name().section('_', 0, 0);
+
     QTranslator translator;
     translator.load(QString("Compact_") + locale);
     app.installTranslator(&translator);
+
+    QTranslator translator2;
+    translator2.load(QString("qt_") + locale, QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    app.installTranslator(&translator2);
     
     FenetrePrincipale fenetre;
     fenetre.show();
